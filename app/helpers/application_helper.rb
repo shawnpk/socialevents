aa3e46aa37e93a54733a5c2a18101540a0ef9040
Shawn Kearney <shawnpk@ineptsoftware.com>
2018-01-06 10:43:40 -0800

    Add helper methods for page titles and date/time format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def full_title(page_title)
+    default_title = 'SocialEvents'
+    if page_title.empty?
+      default_title
+    else
+      "#{page_title} - #{default_title}"
+    end
+  end
+
+  def format_date(date)
+    date.strftime('%A, %d %b %Y %I:%M %p')
+  end
 end

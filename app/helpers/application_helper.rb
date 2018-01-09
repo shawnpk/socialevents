d889be56e84f5c47021867a3e785c40db77b996f
Shawn Kearney <shawnpk@ineptsoftware.com>
2018-01-08 19:28:59 -0800

    Add user show page to show all user events

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
   def format_date(date)
     date.strftime('%A, %d %b %Y %I:%M %p')
   end
+
+  def event_status(event)
+    if event.end_date <= Date.today
+      content_tag(:span, '', class: 'past')
+    else
+      content_tag(:span, '', class: 'upcoming')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'events#index'
+  devise_for :users
+  resources :users, only: [:show]
   resources :events
 end

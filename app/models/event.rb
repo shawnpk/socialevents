ff365bc54dd62024845ceab307462ff6a14d51a6
Shawn Kearney <shawnpk@ineptsoftware.com>
2018-01-09 17:13:25 -0800

    Add admin to users model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,8 @@ class Event < ApplicationRecord
 
   validates       :title,       presence: true,     length: { minimum: 5 }
   validates       :description, presence: true
-  validates_date  :start_date,  :on_or_after =>     :today
-  validates_date  :end_date,    :on_or_after =>     :today
+  # validates_date  :start_date,  :on_or_after =>     :today
+  # validates_date  :end_date,    :on_or_after =>     :today
   validates       :venue,       presence: true
   validates       :location,    presence: true
 end

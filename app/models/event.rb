4e21891a23e6f1f2bab3a7f18be53346db9aadb6
Shawn Kearney <shawnpk@ineptsoftware.com>
2018-01-10 16:07:22 -0800

    Add ability to upload an event image

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,10 @@ class Event < ApplicationRecord
 
   validates       :title,       presence: true,     length: { minimum: 5 }
   validates       :description, presence: true
-  # validates_date  :start_date,  :on_or_after =>     :today
-  # validates_date  :end_date,    :on_or_after =>     :today
+  validates_date  :start_date,  :on_or_after =>     :today, on: :create
+  validates_date  :end_date,    :on_or_after =>     :today, on: :create
   validates       :venue,       presence: true
   validates       :location,    presence: true
+
+  mount_uploader :image, ImageUploader
 end

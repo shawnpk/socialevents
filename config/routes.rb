fe5d1446653fd2a2cde6017c21a0d8cf9cf5e1d4
Shawn Kearney <shawnpk@ineptsoftware.com>
2018-01-12 05:38:40 -0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,16 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root 'application#index'
-    resources :users, only: [:index]
-    resources :categories, except: [:show]
+    resources :users,       only: [:index]
+    resources :categories,  except: [:show]
   end
 
   devise_for  :users
-  resources   :users, only: [:show]
-  resources   :categories, only: [:show]
+  resources   :users,       only: [:show]
+  resources   :categories,  only: [:show]
 
   resources   :events do
-    resources   :comments, only: [:create]
+    resources   :comments,    only: [:create]
     resources   :attendances, only: [:create]
   end
 end

c7ec26cd998ef142264553553fcde9ca6911f403
Shawn Kearney <shawnpk@ineptsoftware.com>
2018-01-13 07:59:27 -0800

    Modify admin dashboard

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -8,6 +8,7 @@ class Admin::ApplicationController < ApplicationController
     @categories   = Category.all
     @comments     = Comment.all
     @attendances  = Attendance.all
+    @likes        = Like.all
   end
 
   private
